78756740ebaa9b04e9a6e45a12a2cbd2812e6c30
wrg2674 <wrg267499@naver.com>
2024-01-04 23:53:21 +0900

    UI버튼과 마이크를 연결, Timer 스크립트 추가

---
+++ CC_Fes/Assets/JGH/scripts/cshTimer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshTimer : MonoBehaviour
+{
+    private float time = 0.0f;
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 300.0f)
+        {
+            time = 0.0f;
+            count++;
+            warning();
+        }
+    }
+    void warning()
+    {
+        int useTime = count * 5;
+        string message = "사용하신지 " + useTime.ToString() + "분이 지났습니다.\n어지럽거나 속이 좋지 않으시다면 기기를 벗고 휴식을 취해주시길 바랍니다.";
+        Debug.Log(message);
+    }
+}
